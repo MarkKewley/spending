79295cd3b571ed5a796855277325a71653a4aa37
Mark Kewley <markkewley@MarkKewleysiMac.roc.mn.charter.com>
2017-08-14 21:38:53 -0500

    header footer, initial start on actions/reducers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
+import {
+    Header,
+    Footer
+} from './components/common';
+
 const SpendingApp = () => {
     return (
         <View style={ styles.container }>
+            <Header/>
             <Text>Hello</Text>
+            <Footer/>
         </View>
     );
 };
